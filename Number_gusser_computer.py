0421b04c2966e892c5ac4769c49eb7d1a24f8e89
Susanta Uprety <sushantuprety165@gmail.com>
2026-09-21 19:54:50 +0100

    Add files via upload

---
+++ Number_gusser_computer.py
@@ -0,0 +1,32 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, You Are Too Low")
+        if guess > random_number:
+            print("You Are Too High")
+        else:
+            print(f"Ya Wow You have guess {random_number} Correctly")
+
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f"Is {guess} too high (H),too low (L), or correct (c)")
+        if feedback == "h":
+            High = guess -1
+        elif feedback == "l":
+            low = guess +1
+    print("Yay! The computer guessed you number")
+
+computer_guess(20)
